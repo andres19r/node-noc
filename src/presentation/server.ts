adcda27e98a00c3be8619cacd5a03d0492164425
Andres Rivero <andres19rivero@gmail.com>
2023-12-19 16:50:05 -0400

    feat: create cron-service file to adapt cron package

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,17 +1,12 @@
-import { CronJob } from "cron";
+import { CronService } from "./cron/cron-service";
 
 export class Server {
   public static start() {
     console.log("Server started...");
 
-    const job = new CronJob(
-      "*/2 * * * * *",
-      () => {
-        const date = new Date()
-        console.log("2 seconds", date);
-      },
-    );
-
-    job.start()
+    CronService.createJob("*/5 * * * * *", () => {
+      const date = new Date();
+      console.log("5 seconds", date);
+    });
   }
 }
