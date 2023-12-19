07746fd481ce34655b63f273a031826928348f2d
Andres Rivero <andres19rivero@gmail.com>
2023-12-19 15:21:05 -0400

    feat!: install & tryout cron package

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,17 @@
+import { CronJob } from "cron";
+
 export class Server {
   public static start() {
     console.log("Server started...");
+
+    const job = new CronJob(
+      "*/2 * * * * *",
+      () => {
+        const date = new Date()
+        console.log("2 seconds", date);
+      },
+    );
+
+    job.start()
   }
 }
