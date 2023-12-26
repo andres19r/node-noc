eb9c43a9284168a8ddf012242ccc0bcd28c98176
Andres Rivero <andres19rivero@gmail.com>
2023-12-26 17:30:39 -0400

    feat: create use-case to log sent emails

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
@@ -9,12 +10,16 @@ const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
 );
 
+const emailService = new EmailService();
+
 export class Server {
   public static start() {
     console.log("Server started...");
 
     // SEND EMAILS
-    // const emailService = new EmailService(fileSystemLogRepository);
+    // new SendEmailLogs(emailService, fileSystemLogRepository).execute(
+    //   "andres19rivero@gmail.com",
+    // );
     // emailService.sendEmailWithFileSystemLogs("andres19rivero@gmail.com");
 
     // CronService.createJob("*/5 * * * * *", () => {
