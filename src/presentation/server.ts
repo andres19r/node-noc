6551c0b9f6eaa352fa7889678aa72ae94a07620e
Andres Rivero <andres19rivero@gmail.com>
2023-12-27 18:08:37 -0400

    feat(datasources): implement & use postgres-log ds

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,12 +4,14 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
 const logRepository = new LogRepositoryImpl(
-  new FileSystemDatasource(),
+  // new FileSystemDatasource(),
+  new PostgresLogDatasource()
   // new MongoLogDatasource()
 );
 
@@ -26,15 +28,15 @@ export class Server {
     // );
     // emailService.sendEmailWithFileSystemLogs("andres19rivero@gmail.com");
 
-    const logs = await logRepository.getLogs(LogSeverityLevel.medium)
-    console.log(logs)
-    // CronService.createJob("*/5 * * * * *", () => {
-    //   const url = "https://adsfadsfasdgoogle.com";
-    //   new CheckService(
-    //     logRepository,
-    //     () => console.log(`${url} is ok`),
-    //     (error) => console.log(error),
-    //   ).execute(url);
-    // });
+    // const logs = await logRepository.getLogs(LogSeverityLevel.low)
+    // console.log(logs)
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = "https://asdfasdgoogle.com";
+      new CheckService(
+        logRepository,
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error),
+      ).execute(url);
+    });
   }
 }
