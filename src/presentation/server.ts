b59791612b3e74ec0106fc46384df77715c2a6c6
Andres Rivero <andres19rivero@gmail.com>
2023-12-28 11:00:48 -0400

    feat: create new check-service to create logs with multiple repositories

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,4 @@
-import { envs } from "../config/plugins/envs.plugin";
-import { LogSeverityLevel } from "../domain/entities/log.entity";
-import { CheckService } from "../domain/use-cases/checks/check-service";
-import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
@@ -9,12 +6,13 @@ import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const logRepository = new LogRepositoryImpl(
-  // new FileSystemDatasource(),
-  new PostgresLogDatasource()
-  // new MongoLogDatasource()
-);
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+
+const postgresLogRepository = new LogRepositoryImpl(
+  new PostgresLogDatasource(),
+);
 
 const emailService = new EmailService();
 
@@ -31,9 +29,9 @@ export class Server {
     // const logs = await logRepository.getLogs(LogSeverityLevel.low)
     // console.log(logs)
     CronService.createJob("*/5 * * * * *", () => {
-      const url = "https://asdfasdgoogle.com";
-      new CheckService(
-        logRepository,
+      const url = "https://google.com";
+      new CheckServiceMultiple(
+        [fsLogRepository, mongoLogRepository, postgresLogRepository],
         () => console.log(`${url} is ok`),
         (error) => console.log(error),
       ).execute(url);
